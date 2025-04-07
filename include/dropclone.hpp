997b1224b1f2fd5ef68233abab54e44bede90be1
Adam Czapla <adam@adamczapla.de>
2025-04-07 15:19:20 +0200

    create minimum project structure

---
+++ include/dropclone.hpp
@@ -0,0 +1,1 @@
+static inline int glob = 77;
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include <dropclone.hpp>
+
+auto main(int argc, char const* argv[]) -> int {
+  std::cout << "hello drop-clone!\n";
+  std::cout << glob << '\n';
+  return EXIT_SUCCESS;
+}
