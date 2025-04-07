54fea6962c886ce23aff3a57c659280f731dfad4
Adam Czapla <adam@adamczapla.de>
2025-04-07 18:45:19 +0200

    add file_info type and its std::hash specialization

--- include/dropclone.hpp
+++ include/dropclone.hpp
@@ -1,1 +1,35 @@
-static inline int glob = 77;
+#pragma once
+
+#include <filesystem>
+#include <unordered_map>
+
+namespace dropclone {
+
+namespace fs = std::filesystem;
+
+struct file_info {
+  fs::file_time_type last_write_time{};
+  uintmax_t file_size{};
+  fs::perms file_perms{};
+
+  auto operator==(file_info const&) const -> bool = default;
+};
+
+} // dropclone
+
+namespace std {
+
+template <> struct hash<dropclone::file_info> {
+  auto operator()(dropclone::file_info const& info) const noexcept -> size_t {
+    size_t seed{0};
+    auto hasher = [&](auto const& hash_value) -> size_t {
+      return hash_value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    };
+    seed ^= hasher(hash<uintmax_t>{}(info.file_size));
+    seed ^= hasher(hash<filesystem::perms>{}(info.file_perms));
+    seed ^= hasher(hash<int64_t>{}(info.last_write_time.time_since_epoch().count()));
+    return seed;
+  }
+};
+
+} // std
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <filesystem>
 #include <cstdlib>
 #include <dropclone.hpp>
 
 auto main(int argc, char const* argv[]) -> int {
-  std::cout << "hello drop-clone!\n";
-  std::cout << glob << '\n';
+  namespace fs = std::filesystem;
+  namespace dc = dropclone;
+
+  std::cout << fs::current_path() << '\n';
+  fs::path p{"dropclone"};
+  dc::file_info fi{fs::last_write_time(p), fs::file_size(p), fs::status(p).permissions()};
+  std::cout << std::hash<dc::file_info>{}(fi) << '\n';
+  
   return EXIT_SUCCESS;
 }
