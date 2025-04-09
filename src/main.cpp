23119bbf2321bc72e4fc0f91d2842ef170176ca6
Adam Czapla <adam@adamczapla.de>
2025-04-09 13:08:25 +0200

    test commit after changing the repository name from drop-clone to dropclone

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ auto main(int argc, char const* argv[]) -> int {
   namespace fs = std::filesystem;
   namespace dc = dropclone;
 
+  std::cout << "test\n";
+
   std::cout << fs::current_path() << '\n';
   fs::path p{"dropclone"};
   dc::path_info fi{fs::last_write_time(p), fs::file_size(p), fs::status(p).permissions()};
