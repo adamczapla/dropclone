40a7c56a77fbd74d13e724c6b527ee49f1fbbf54
Adam Czapla <adam@adamczapla.de>
2025-04-13 11:54:22 +0200

    format code

--- src/drop_clone.cpp
+++ src/drop_clone.cpp
@@ -8,19 +8,12 @@
 
 namespace dropclone {
 
-// maybe is config_path not neccessary as member
 drop_clone::drop_clone(fs::path config_path, config_parser parser) noexcept 
-    : config_path_{std::move(config_path)}, parser_{std::move(parser)} {
-
+    : config_path_{std::move(config_path)}, parser_{std::move(parser)} { 
   try {
     clone_config_ = parser_(config_path_);
-
-    for (auto& entry : clone_config_.entries) {
-      entry.sanitize();
-    }
-
+    for (auto& entry : clone_config_.entries) { entry.sanitize(); }
     clone_config_.validate();
-
   } catch (dropclone::exception const& e) {
     std::cerr << e.what();
     std::exit(EXIT_FAILURE);
