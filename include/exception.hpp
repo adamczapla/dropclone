983ec280acb4d777b7affb13c6c326e8f09d9fa6
Adam Czapla <adam@adamczapla.de>
2025-04-13 11:49:05 +0200

    add config error codes: path_not_absolute, overlapping_path_conflict

--- include/exception.hpp
+++ include/exception.hpp
@@ -7,10 +7,13 @@
 namespace dropclone::errorcode {
 
 namespace config { 
-  inline constexpr auto file_not_found   = "config_error.001";
-  inline constexpr auto parse_error      = "config_error.002";
-  inline constexpr auto conversion_error = "config_error.003";
-  inline constexpr auto invalid_mode     = "config_error.004";
+  inline constexpr auto file_not_found            = "config_error.001";
+  inline constexpr auto parse_error               = "config_error.002";
+  inline constexpr auto conversion_error          = "config_error.003";
+  inline constexpr auto path_not_absolute         = "config_error.004";
+  inline constexpr auto invalid_clone_mode        = "config_error.005";
+  inline constexpr auto overlapping_path_conflict = "config_error.005";
+
 } // namespace dropclone::errorcode::config
 
 namespace filesystem {} // namespace dropclone::errorcode::filesystem
