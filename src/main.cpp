948284e63f1fec3b50de622ace7503ff78038032
Adam Czapla <adam@adamczapla.de>
2025-04-16 20:12:24 +0200

    add drop_clone constructor call and info-level logs indicating starting and terminating the program

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,32 @@
+#include <drop_clone.hpp>
+#include <logger_manager.hpp>
+#include <parsers/nlohmann_json_parser.hpp>
+
 #include <iostream>
 #include <filesystem>
 #include <cstdlib>
-#include <clone_manager.hpp>
-#include <nlohmann/json.hpp>
-#include <spdlog/spdlog.h>
+
+namespace fs = std::filesystem;
+namespace dc = dropclone;
+
+using json = nlohmann::json;
+using dropclone::logger;
+using dropclone::logger_id;
+using dropclone::drop_clone;
+using dropclone::nlohmann_json_parser;
 
 auto main(int argc, char const* argv[]) -> int {
-  namespace fs = std::filesystem;
-  namespace dc = dropclone;
 
-  std::cout << "test\n";
+  static constexpr char const* config_file{"/Users/adamc/github/dropclone/config/dropclone.json"};
+  
+  logger.get(logger_id::core)->info("dropclone starting...");
+  drop_clone clone{config_file, nlohmann_json_parser{}};
+  logger.get(logger_id::core)->info("dropclone terminated.");
 
-  std::cout << fs::current_path() << '\n';
-  fs::path p{"dropclone"};
-  dc::path_info fi{fs::last_write_time(p), fs::file_size(p), fs::status(p).permissions()};
-  std::cout << std::hash<dc::path_info>{}(fi) << '\n';
+  // std::cout << fs::current_path() << '\n';
+  // fs::path p{"dropclone"};
+  // dc::path_info fi{fs::last_write_time(p), fs::file_size(p), fs::status(p).permissions()};
+  // std::cout << std::hash<dc::path_info>{}(fi) << '\n';
   
   return EXIT_SUCCESS;
 }
