b5b1a548827dacf89e47e9aadc9925782e1e9783
Adam Czapla <adam@adamczapla.de>
2025-04-16 07:51:49 +0200

    add init_startup_logger(), init_task_logger(), and init_daemon_logger() declarations

--- include/drop_clone.hpp
+++ include/drop_clone.hpp
@@ -13,6 +13,10 @@ class drop_clone {
   drop_clone(fs::path config_path, config_parser) noexcept;
 
  private:
+  auto init_startup_logger() -> void;
+  auto init_task_logger() -> void;
+  auto init_daemon_logger() -> void;
+
   fs::path config_path_; // maybe i'll remove it later
   config_parser parser_; // maybe i'll remove it later
   clone_config clone_config_;
