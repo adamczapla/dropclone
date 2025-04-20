a3eb5146f1a322f63a8b25117c06a3892c4c3479
Adam Czapla <adam@adamczapla.de>
2025-04-20 16:04:31 +0200

    remove output operator declaration for clone_mode and unnecessary includes

--- include/dropclone/clone_config.hpp
+++ include/dropclone/clone_config.hpp
@@ -3,19 +3,15 @@
 #include <nlohmann/json.hpp>
 #include <filesystem>
 #include <functional>
-#include <optional>
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <ostream>
 
 namespace dropclone {
 
 namespace fs = std::filesystem;
 
 enum class clone_mode { copy, move, undefined };
-
-auto operator<<(std::ostream&, clone_mode const&) -> std::ostream&;
 
 NLOHMANN_JSON_SERIALIZE_ENUM(clone_mode, {
   {clone_mode::undefined, "undefined"},
