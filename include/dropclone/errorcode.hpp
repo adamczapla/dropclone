c8a08d04a8aa1c19f3d6b04fc4775d96149ca3c6
Adam Czapla <adam@adamczapla.de>
2025-04-21 19:53:06 +0200

    add new errorcodes missing_required_field and no_entries_defined

--- include/dropclone/errorcode.hpp
+++ include/dropclone/errorcode.hpp
@@ -13,6 +13,8 @@ struct config {
   static constexpr auto invalid_clone_mode        = "config_error.005";
   static constexpr auto overlapping_path_conflict = "config_error.006";
   static constexpr auto path_not_configured       = "config_error.007";
+  static constexpr auto missing_required_field    = "config_error.008";
+  static constexpr auto no_entries_defined        = "config_error.010";
   
   static inline std::unordered_map<std::string_view, std::string_view> const messages{
     {file_not_found, "cannot open config file: {}"},
@@ -21,7 +23,9 @@ struct config {
     {path_not_absolute, "'{}' must be an absolte path"},
     {invalid_clone_mode, "'{}' must be (copy or move)"},
     {overlapping_path_conflict, "Overlapping path detected in '{}': {}"},
-    {path_not_configured, "{} path is not configured or not absolute – using fallback: '{}'"}
+    {path_not_configured, "{} path is not configured or not absolute – using fallback: '{}'"},
+    {missing_required_field, "missing required field: '{}' in config file '{}'"},
+    {no_entries_defined, "no entries defined in config file '{}'"}
   };
 };
 
