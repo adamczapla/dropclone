eb1cd338962bbbf777b3d733fc2ed9ed618230cd
Adam Czapla <adam@adamczapla.de>
2025-04-28 10:49:02 +0200

    expand path_info.hpp - add enum class path_conflict_t - add path_conflict messagecode type for path_conflict_t - add to_string overload for path_conflict_t - add member of type path_conflict_t to path_info class

--- include/dropclone/path_info.hpp
+++ include/dropclone/path_info.hpp
@@ -1,16 +1,57 @@
 #pragma once
 
+#include <dropclone/messagecode.hpp>
+#include <dropclone/utility.hpp>
 #include <filesystem>
 #include <unordered_map>
+#include <string>
 
 namespace dropclone {
 
 namespace fs = std::filesystem;
+
+enum class path_conflict_t{none, size_mismatch, permission_mismatch, permission_denied};
+
+namespace messagecode {
+
+struct path_conflict {
+  static constexpr auto size_mismatch         = "path_conflict_message.001";
+  static constexpr auto permission_mismatch   = "path_conflict_message.002";
+  static constexpr auto permission_denied     = "path_conflict_message.003";
+
+  static inline std::unordered_map<path_conflict_t, std::string_view> const messages{
+    {path_conflict_t::size_mismatch, "file sizes differ for '{}' despite identical timestamps"},
+    {path_conflict_t::permission_mismatch, "file permissions differ for '{}'"},
+    {path_conflict_t::permission_denied, "file permissions denied for '{}'"}
+  };
+};
+  
+} // namespace messagecode
+
+namespace utility {
+
+inline auto to_string(path_conflict_t conflict) -> std::string {
+  namespace msc = messagecode;
+  switch (conflict) {
+    case path_conflict_t::none: 
+      return std::string{"no conflict"};
+    case path_conflict_t::size_mismatch:
+      return std::string{msc::path_conflict::size_mismatch};
+    case path_conflict_t::permission_mismatch:
+      return std::string{msc::path_conflict::permission_mismatch};
+    case path_conflict_t::permission_denied:
+      return std::string{msc::path_conflict::permission_denied};
+  }
+  return "unknown conflict";
+}
+
+} // namespace utility
   
 struct path_info {
   fs::file_time_type last_write_time{};
   uintmax_t file_size{};
   fs::perms file_perms{};
+  path_conflict_t conflict{path_conflict_t::none};
   
   auto operator==(path_info const&) const -> bool = default;
 };
