3048b12a43ca5119c18743dd53f1902fdb053cf6
Adam Czapla <adam@adamczapla.de>
2025-04-28 10:42:52 +0200

    make formatter more generic - replace std::string_view with auto for parameter 'code' - add to_string overload for std::string_view

--- include/dropclone/utility.hpp
+++ include/dropclone/utility.hpp
@@ -6,15 +6,17 @@
 
 namespace dropclone::utility {
 
+inline auto to_string(std::string_view code) -> std::string { return std::string{code}; }
+
 template <typename message_type>
 struct formatter {
-  static auto format(std::string_view code, auto const&... args) -> std::string {
+  static auto format(auto code, auto const&... args) -> std::string {
     try {
       return std::vformat(message_type::messages.at(code), std::make_format_args(args...));
     } catch (std::format_error const& e) {
-      return std::format("[format_error: {} ] for code {}", e.what(), code);
+      return std::format("[format_error: {} ] for code {}", e.what(), utility::to_string(code));
     } catch (std::out_of_range const&) {
-      return std::format("[missing_message_code: {}]", code);
+      return std::format("[missing_message_code: {}]", utility::to_string(code));
     }
   }
 };
