1ab059acaffdd9c649c78e69c515d0763599fb8d
Adam Czapla <adam@adamczapla.de>
2025-04-28 10:30:57 +0200

    add vector of clone_managers to members

--- include/dropclone/drop_clone.hpp
+++ include/dropclone/drop_clone.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <dropclone/clone_config.hpp>
+#include <dropclone/clone_manager.hpp>
 #include <filesystem>
+#include <vector>
 
 namespace dropclone {
 
@@ -10,14 +12,15 @@ namespace fs = std::filesystem;
 class drop_clone {
  public:
   drop_clone(fs::path config_path, config_parser);
+  auto sync() -> void;
 
  private:
   auto init_config_logger() -> void;
   auto init_startup_logger() -> void;
   auto init_daemon_logger() -> void;
 
-  config_parser parser_;
   clone_config clone_config_;
+  std::vector<clone_manager> managers_{};
 };
   
 } // namespace dropclone
