6e244e26ad33367fe271380e726ff06ba682868b
Adam Czapla <adam@adamczapla.de>
2025-05-01 20:57:08 +0200

    add second clone.sync() call executed 30 seconds after the first

--- src/run_main.cpp
+++ src/run_main.cpp
@@ -18,18 +18,19 @@ using dropclone::logger_id;
 using dropclone::drop_clone;
 using dropclone::nlohmann_json_parser;
 
+namespace rng = std::ranges;
 namespace vws = std::views;
 
 auto run_main(int argc, char const* argv[]) -> int {
 
   static constexpr char const* config_file{"/Users/adamc/github/dropclone/config/dropclone.json"};
-  
+
   logger.get(logger_id::core)->info("dropclone starting...");
   try {
     drop_clone clone{config_file, nlohmann_json_parser{}};
     clone.sync();
-    // std::this_thread::sleep_for(std::chrono::minutes{1});
-    // clone.sync();
+    std::this_thread::sleep_for(std::chrono::seconds{30});
+    clone.sync();
   } catch (dc::exception const& e) {}
   logger.get(logger_id::core)->info("dropclone terminated.");
   
