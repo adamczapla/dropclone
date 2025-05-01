7982ffdeb02353aacea4809bcf67662812c7955c
Adam Czapla <adam@adamczapla.de>
2025-05-01 21:02:34 +0200

    add members files_ and directories_ with getters files() and directories() - add member functions: add_files, add_directories, clear_directories, rebase, entries - add type aliases: snapshot_directories and entry_filter

--- include/dropclone/path_snapshot.hpp
+++ include/dropclone/path_snapshot.hpp
@@ -2,9 +2,11 @@
 
 #include <dropclone/path_info.hpp>
 #include <unordered_map>
+#include <map>
 #include <filesystem>
 #include <functional>
 #include <unordered_set> 
+#include <set>
 
 namespace dropclone {
 
@@ -13,8 +15,10 @@ namespace dropclone {
   class path_snapshot {
    public:
     using snapshot_entries = std::unordered_map<fs::path, path_info>; 
+    using snapshot_directories = std::map<fs::path, path_info>;
     using uncertain_processing_paths = std::unordered_set<fs::path>;
     using path_filter = std::function<bool(fs::path const&)>;
+    using entry_filter = std::function<bool(snapshot_entries::value_type const&)>;
   
     explicit path_snapshot(fs::path root);
   
@@ -23,11 +27,22 @@ namespace dropclone {
 
     inline auto root() const noexcept -> fs::path;
     inline auto hash() const noexcept -> size_t;
+    inline auto entries() const noexcept -> snapshot_entries const&;
+    inline auto files() const noexcept -> snapshot_entries const&;
+    inline auto directories() const noexcept -> snapshot_directories const&;
+
+    auto add_files(snapshot_entries const& files, entry_filter filter) -> void;
+    auto add_directories(snapshot_directories const& directories, entry_filter filter) -> void;
+    inline auto clear_directories() -> void;
+
+    auto rebase(fs::path const& new_root) -> void;
   
    private:
     fs::path root_;
     snapshot_entries entries_{};
     snapshot_entries conflicts_{};
+    snapshot_entries files_{};
+    snapshot_directories directories_{};
     uncertain_processing_paths uncertain_processing_paths_{};
     size_t hash_{};
   
@@ -36,5 +51,9 @@ namespace dropclone {
 
   auto path_snapshot::root() const noexcept -> fs::path { return root_; }
   auto path_snapshot::hash() const noexcept -> size_t { return hash_; }
+  auto path_snapshot::entries() const noexcept -> snapshot_entries const& { return entries_; }
+  auto path_snapshot::files() const noexcept -> snapshot_entries const& { return files_; }
+  auto path_snapshot::directories() const noexcept -> snapshot_directories const& { return directories_; }
+  auto path_snapshot::clear_directories() -> void { snapshot_directories{}.swap(directories_); }
 
 } // namespace dropclone
