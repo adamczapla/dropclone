d90977a143732d859fc1722c00209059bd8285f8
Adam Czapla <adam@adamczapla.de>
2025-05-03 18:11:57 +0200

    add errorcode type command and move copy_command_failed, rename_command_failed, remove_command_failed errorcodes to it

--- include/dropclone/errorcode.hpp
+++ include/dropclone/errorcode.hpp
@@ -34,16 +34,22 @@ struct config {
 struct filesystem {
   static constexpr auto could_not_create_directory    = "filesystem_error.001";
   static constexpr auto failed_to_traverse_directory  = "filesystem_error.002";
-  static constexpr auto copy_command_failed           = "filesystem_error.003";
-  static constexpr auto rename_command_failed         = "filesystem_error.004";
-  static constexpr auto remove_command_failed         = "filesystem_error.005";
   
   static inline std::unordered_map<std::string_view, std::string_view> const messages{
     {could_not_create_directory, "could not create directory: {} |\n↳ origin error: \n\t↳ {}"},
-    {failed_to_traverse_directory, "failed to traverse directory: {} in {} |\n↳ origin error: \n\t↳ {}"},
-    {copy_command_failed, "copy_command ({}): '{}' → '{}' failed |\n↳ origin error:\n\t↳ {}"},
-    {rename_command_failed, "rename_command ({}): '{}' → '{}' failed |\n↳ origin error:\n\t↳ {}"},
-    {remove_command_failed, "remove_command ({}): '{}' → '{}' failed |\n↳ origin error:\n\t↳ {}"}
+    {failed_to_traverse_directory, "failed to traverse directory: {} in {} |\n↳ origin error: \n\t↳ {}"}
+  };
+};
+
+struct command {
+  static constexpr auto copy_command_failed           = "command_error.001";
+  static constexpr auto rename_command_failed         = "command_error.002";
+  static constexpr auto remove_command_failed         = "command_error.003";
+  
+  static inline std::unordered_map<std::string_view, std::string_view> const messages{
+    {copy_command_failed, "copy_command::{}: '{}' → '{}' failed |\n↳ origin error:\n\t↳ {}"},
+    {rename_command_failed, "rename_command::{}: '{}' → '{}' failed |\n↳ origin error:\n\t↳ {}"},
+    {remove_command_failed, "remove_command::{}: '{}' failed |\n↳ origin error:\n\t↳ {}"}
   };
 };
 
