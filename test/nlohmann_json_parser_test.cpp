3a2959b07040edcd579140e65e5c719573c64511
Adam Czapla <adam@adamczapla.de>
2025-05-16 21:33:57 +0200

    add test cases for both exclude and include fields

--- test/nlohmann_json_parser_test.cpp
+++ test/nlohmann_json_parser_test.cpp
@@ -140,6 +140,70 @@ TEST_CASE("parser throws if required field 'mode' is missing", "[nlohmann_json_p
     Catch::Matchers::MessageMatches(Catch::Matchers::ContainsSubstring("config_error.008")));
 }
 
+TEST_CASE("parser throws if both 'exclude' and 'include' fields are set", "[nlohmann_json_parser]") {
+  constexpr auto json_config = 
+  R"(
+  {
+    "clone_config" : [ 
+      {
+        "source_directory" : "/home/source",
+        "destination_directory" : "/home/destination/",
+        "mode" : "copy",
+        "exclude" : [],
+        "include" : []
+      }
+    ],
+    "log_directory" : "/github/dropclone/log/"
+  })";
+
+  create_temporary_json_file(json_config);
+
+  REQUIRE_THROWS_MATCHES(dc::nlohmann_json_parser{}(temp_config_path), dc::exception, 
+    Catch::Matchers::MessageMatches(Catch::Matchers::ContainsSubstring("config_error.011")));
+}
+
+TEST_CASE("parser throws if field 'exclude' has invalid type", "[nlohmann_json_parser]") {
+  constexpr auto json_config = 
+  R"(
+  {
+    "clone_config" : [ 
+      {
+        "source_directory" : "/home/source",
+        "destination_directory" : "/home/destination/",
+        "mode" : "copy",
+        "exclude" : "pattern$" 
+      }
+    ],
+    "log_directory" : "/github/dropclone/log/"
+  })";
+
+  create_temporary_json_file(json_config);
+
+  REQUIRE_THROWS_MATCHES(dc::nlohmann_json_parser{}(temp_config_path), dc::exception, 
+    Catch::Matchers::MessageMatches(Catch::Matchers::ContainsSubstring("config_error.010")));
+}
+
+TEST_CASE("parser throws if field 'include' has invalid type", "[nlohmann_json_parser]") {
+  constexpr auto json_config = 
+  R"(
+  {
+    "clone_config" : [ 
+      {
+        "source_directory" : "/home/source",
+        "destination_directory" : "/home/destination/",
+        "mode" : "copy",
+        "include" : "pattern$" 
+      }
+    ],
+    "log_directory" : "/github/dropclone/log/"
+  })";
+
+  create_temporary_json_file(json_config);
+
+  REQUIRE_THROWS_MATCHES(dc::nlohmann_json_parser{}(temp_config_path), dc::exception, 
+    Catch::Matchers::MessageMatches(Catch::Matchers::ContainsSubstring("config_error.010")));
+}
+
 TEST_CASE("parser throws if field 'log_directory' has invalid type", "[nlohmann_json_parser]") {
   constexpr auto json_config = 
   R"(
@@ -236,12 +300,14 @@ TEST_CASE("parser passes if multiple entries are configured correctly", "[nlohma
       {
         "source_directory" : "/home/source",
         "destination_directory" : "/home/destination/",
-        "mode" : "copy"
+        "mode" : "copy",
+        "exclude" : ["pattern1", "pattern2"]
       },
       {
         "source_directory" : "/home/source2",
         "destination_directory" : "/home/destination2/",
-        "mode" : "move"
+        "mode" : "move",
+        "include" : ["pattern1", "pattern2"]
       }
     ],
     "log_directory" : "/github/dropclone/log/"
